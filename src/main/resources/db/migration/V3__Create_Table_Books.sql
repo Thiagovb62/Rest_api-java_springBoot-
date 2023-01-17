4a5693a6f65a77b1b0cf86a9fe35099f3a852898
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2023-01-17 10:51:04 -0300

    criando a migration de Book

---
+++ src/main/resources/db/migration/V3__Create_Table_Books.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `books` (
+                         `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+                         `author` longtext,
+                         `price` decimal(65,2) NOT NULL,
+                         `title` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
